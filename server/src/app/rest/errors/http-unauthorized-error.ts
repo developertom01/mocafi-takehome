a4be688db13c921ec6043860ac797be5bbcd8f41
developertom <developertom01@github.com>
2024-12-31 08:17:47 +0000

    Removed all nodejs files

--- server/src/app/rest/errors/http-unauthorized-error.ts
+++
@@ -1,7 +0,0 @@
-import { HttpBaseError } from ".";
-
-export class HttpUnauthorizedError extends HttpBaseError {
-  constructor(message: string, data?: object | string) {
-    super(401, message, data);
-  }
-}
--- server/src/errors/base-error.ts
+++
@@ -1,6 +0,0 @@
-export abstract class BaseError extends Error {
-  constructor(message: string, public readonly data?: object | string) {
-    super(message);
-    this.name = this.constructor.name;
-  }
-}
