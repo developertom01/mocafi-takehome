7f705e93bef39ae8ed4bea3ea1d2d86a52b9e246
developertom <developertom01@github.com>
2025-01-04 03:00:42 +0000

    Decode user data in Linode instance configuration for improved handling

--- infra/iac/main.tf
+++ infra/iac/main.tf
@@ -37,7 +37,7 @@ resource "linode_instance" "mocafi-server" {
 
   stackscript_id = linode_stackscript.start-server.id
   metadata {
-    user_data = file("setup.sh")
+    user_data = base64decode(file("setup.sh"))
   }
 
 }
